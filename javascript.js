1a880b20255a3a1d0d245ee5953a70d97016ba9a
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-04-27 21:18:49 +0800

    rizz

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('button')
+
+function clicked() {
+window.alert('This device has been compromised. Have a good day!')
+}
+
+button.addEventListener('click', clicked)
