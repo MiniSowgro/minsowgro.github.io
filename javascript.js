b57a1145959afcb9c58e27e66731ce7a0f0c01d8
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-05-01 11:20:01 +0800

    Update javascript.js
    Added randomNum variable, and added console.log(randomNum)

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 var button = document.getElementById('button')
 
+let randomNum = math.random() * 100;
+
 function clicked() {
 window.alert('This device has been compromised. Have a good day!')
+console.log(randomNum)
 }
 
 button.addEventListener('click', clicked)
