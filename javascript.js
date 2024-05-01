a1ab308334c1b7833d4b510c4bbc75f5ac59c693
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-05-01 12:02:18 +0800

    Update javascript.js
    Added https:// to window.open

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ var button = document.getElementById('button')
 let randomNum = math.random() * 100;
 
 function clicked() {
-window.open("youtube.com")
+window.open("https://youtube.com")
 console.log(randomNum)
 }
 
