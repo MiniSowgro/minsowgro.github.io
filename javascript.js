25996001259b0335d11ddb6a2dabdbf9023f2597
MiniSowgro <132415164+MiniSowgro@users.noreply.github.com>
2024-05-01 11:22:17 +0800

    Update javascript.js
    Changed window.alert to window.open

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ var button = document.getElementById('button')
 let randomNum = math.random() * 100;
 
 function clicked() {
-window.alert('This device has been compromised. Have a good day!')
+window.open("youtube.com")
 console.log(randomNum)
 }
 
